0edf69997f02614655d295c7a39f464e77683223
Justin <49623256+justinshwang@users.noreply.github.com>
2021-02-06 17:09:16 -0500

    created chore model status method

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,6 +2,9 @@ class Chore < ApplicationRecord
     belongs_to :task
     belongs_to :child
 
+    # validations
+    validates_date :due_on
+
     # scopes
     scope :by_task, -> { joins(:task).order("name") }
     scope :chronological, -> { order("due_on") }
@@ -10,4 +13,12 @@ class Chore < ApplicationRecord
     scope :done, -> { where(completed: true) }
     scope :upcoming, -> { where('due_on >= ?', Date.today) }
     scope :past, -> { where('due_on < ?', Date.today) }
+
+    # methods
+    def status 
+        if self.completed == true 
+            return "Completed"
+        end
+        return "Pending"
+    end
 end
