e083e8891d455463bf1d44420c0ede3b01699e32
Justin <49623256+justinshwang@users.noreply.github.com>
2021-02-06 16:38:44 -0500

    added task model validations and scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,13 @@
 class Task < ApplicationRecord
+    # relationships
     has_many :chores
     has_many :children, through: :chores
+    
+    # validations
+    validates_presence_of :name 
+    validates_numericality_of :points, :greater_than_or_equal_to => 0, :only_integer => true
 
+    # scopes
+    scope :alphabetical, -> { order("name") }
+    scope :active, -> { where(active: true) }
 end
