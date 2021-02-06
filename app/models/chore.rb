d3604bc2e835438130949299c70383b4d3a9bcf9
Justin <49623256+justinshwang@users.noreply.github.com>
2021-02-06 17:05:40 -0500

    added scopes for chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,13 @@
 class Chore < ApplicationRecord
     belongs_to :task
     belongs_to :child
+
+    # scopes
+    scope :by_task, -> { joins(:task).order("name") }
+    scope :chronological, -> { order("due_on") }
+
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where('due_on >= ?', Date.today) }
+    scope :past, -> { where('due_on < ?', Date.today) }
 end
