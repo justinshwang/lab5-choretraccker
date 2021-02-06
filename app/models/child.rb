e3dbe4aa20aadafa42d706b70b6b1b4083add86e
Justin <49623256+justinshwang@users.noreply.github.com>
2021-02-06 17:13:34 -0500

    added points_earned method to child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,10 +5,16 @@ class Child < ApplicationRecord
     validates_presence_of :first_name
     validates_presence_of :last_name 
 
+    # methods
+    
     # returns "First Last"
     def name 
         first_name + " " + last_name
     end
+    # returns points earned from chores
+    def points_earned
+        self.chores.done.inject(0){|sum,chore| sum += chore.task.points}
+    end 
 
     # scopes
     scope :alphabetical, -> { order("last_name", "first_name") }
